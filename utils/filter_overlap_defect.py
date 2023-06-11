496958a5a436ddb83c063b10e465790479d3d8e2
beamvilla <villa.beam@gmail.com>
2023-06-11 19:42:41 +0700

    fix : array bug

--- utils/filter_overlap_defect.py
+++ utils/filter_overlap_defect.py
@@ -4,38 +4,46 @@ from utils.non_max_suppression import non_max_suppression
 
 
 def filter(pred_results):
-    filtered_rois = []
-    filtered_class_ids = []
-    filtered_scores = []
-    filtered_masks = []
+    skin_id = 1
+    n_skin = 0
+
+    for c in pred_results["class_ids"]:
+        if c == skin_id:
+            n_skin += 1
+
+    h, w, c = pred_results["masks"].shape
 
     without_skin_rois = []
     without_skin_class_ids = []
     without_skin_scores = []
-    without_skin_masks = []
+    without_skin_masks = np.zeros((h, w, c - n_skin))
 
     skin_rois = []
     skin_class_ids = []
     skin_scores = []
-    skin_masks = []
+    skin_masks = np.zeros((h, w, n_skin))
 
-    skin_id = 1
-    for i, c in enumerate(pred_results["class_ids"]):
-        if c != skin_id:
+    nxt_defect = 0
+    nxt_skin = 0
+
+    for i in range(len(pred_results["class_ids"])):
+        if pred_results["class_ids"][i] != skin_id:
             without_skin_rois.append(pred_results["rois"][i])
             without_skin_class_ids.append(pred_results["class_ids"][i])
             without_skin_scores.append(pred_results["scores"][i])
-            without_skin_masks.append(pred_results["masks"][:, :, i])
+            without_skin_masks[:, :, nxt_defect] = pred_results["masks"][:, :, i]
+            nxt_defect += 1
         else:
             skin_rois.append(pred_results["rois"][i])
             skin_class_ids.append(pred_results["class_ids"][i])
             skin_scores.append(pred_results["scores"][i])
-            skin_masks.append(pred_results["masks"][:, :, i])
+            skin_masks[:, :, nxt_skin] = pred_results["masks"][:, :, i]
+            nxt_skin += 1
 
     without_skin_rois = np.array(without_skin_rois)
     without_skin_class_ids = np.array(without_skin_class_ids)
     without_skin_scores = np.array(without_skin_scores)
-    without_skin_masks = np.array(without_skin_masks)        
+    without_skin_masks = np.array(without_skin_masks)
 
     skin_rois = np.array(skin_rois)
     skin_class_ids = np.array(skin_class_ids)
@@ -47,23 +55,31 @@ def filter(pred_results):
                     max_bbox_overlap=0.75,
                     scores=without_skin_scores
                 )
-    
+
+    filtered_rois = []
+    filtered_class_ids = []
+    filtered_scores = []
+    filtered_masks = np.zeros((h, w, c))
+
+    nxt = 0
     for idx in filtered_ids:
         filtered_rois.append(without_skin_rois[idx])
         filtered_class_ids.append(without_skin_class_ids[idx])
         filtered_scores.append(without_skin_scores[idx])
-        filtered_masks.append(without_skin_masks[:, :, idx])
-        
-    filtered_rois.extend(skin_rois)
-    filtered_class_ids.extend(skin_class_ids)
-    filtered_scores.extend(skin_scores)
-    filtered_masks.extend(skin_masks)
+        filtered_masks[:, :, nxt] = without_skin_masks[:, :, idx]
+        nxt += 1
 
-    filtered_masks = np.moveaxis(filtered_masks, -1, 0)
+    for i in range(len(skin_rois)):
+        filtered_rois.append(skin_rois[i])
+        filtered_class_ids.append(skin_class_ids[i])
+        filtered_scores.append(skin_scores[i])
+        filtered_masks[:, :, nxt] = skin_masks[:, :, i]
+        nxt += 1
+
     filtered_pred_results = {
-        "rois": np.array(filtered_rois),
-        "class_ids": np.array(filtered_class_ids),
-        "scores": np.array(filtered_scores),
-        "masks": np.array(filtered_masks)
-    }
+            "rois": np.array(filtered_rois),
+            "class_ids": np.array(filtered_class_ids),
+            "scores": np.array(filtered_scores),
+            "masks": np.array(filtered_masks)
+        }
     return filtered_pred_results
