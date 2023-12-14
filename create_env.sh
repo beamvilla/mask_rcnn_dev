35af685e284807c30474d0dce57299b6b5672f7b
beamvilla <villa.beam@gmail.com>
2023-12-14 12:33:59 +0700

    fix: conda argument

--- create_env.sh
+++ create_env.sh
@@ -5,7 +5,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh -b -f -p /usr/local
 conda install -q -y --prefix /usr/local python=3.7 ujson
-conda install -y -c cudatoolkit=10.1
+conda install -c cudatoolkit=10.1
 cp /content/drive/MyDrive/libcudnn7_7.6.5.32-1+cuda10.0_amd64.deb .
 sudo dpkg -i /content/drive/MyDrive/libcudnn7_7.6.5.32-1+cuda10.0_amd64.deb
 python3 /content/mask_rcnn_dev/activate_conda.py
