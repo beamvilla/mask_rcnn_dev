a4cccd78fcbf2f0777650350e15e9dce2aac4279
beamvilla <villa.beam@gmail.com>
2023-12-14 12:20:21 +0700

    fix: script

--- create_env.sh
+++ create_env.sh
@@ -7,8 +7,8 @@ chmod +x Miniconda3-latest-Linux-x86_64.sh
 conda install -q -y --prefix /usr/local python=3.7 ujson
 conda install -y -c cudatoolkit=10.1
 cp /content/drive/MyDrive/libcudnn7_7.6.5.32-1+cuda10.0_amd64.deb .
-sudo dpkg -i libcudnn7_7.6.5.32-1+cuda10.1_amd64.deb
-python3 ./activate_conda.py
+sudo dpkg -i /content/drive/MyDrive/libcudnn7_7.6.5.32-1+cuda10.1_amd64.deb
+python3 /content/mask_rcnn_dev/activate_conda.py
 python --version
 sudo apt install unzip
-pip install tensorflow-gpu==1.14.0 keras==2.2.5 scikit-image==0.16.2 h5py==2.10.0 matplotlib datetime pytest-shutil pandas ipykernel opencv- protobuf==3.20.0
+pip install tensorflow-gpu==1.14.0 keras==2.2.5 scikit-image==0.16.2 h5py==2.10.0 matplotlib datetime pytest-shutil pandas ipykernel opencv-python protobuf==3.20.0
